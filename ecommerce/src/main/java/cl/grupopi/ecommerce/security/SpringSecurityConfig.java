a1d52d0c9417a724db313d1a424866d826f01c32
Christian montoya <christian.montoya@systemproject.cl>
2024-01-09 14:36:10 -0300

    feat: se agrega authenticationManager para springsecurityconfig

--- ecommerce/src/main/java/cl/grupopi/ecommerce/security/SpringSecurityConfig.java
+++ ecommerce/src/main/java/cl/grupopi/ecommerce/security/SpringSecurityConfig.java
@@ -1,7 +1,11 @@
 package cl.grupopi.ecommerce.security;
 
+import cl.grupopi.ecommerce.security.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,16 +15,22 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests( (authz) -> authz
                         .requestMatchers("/api/v1/users").permitAll()
                         .anyRequest().authenticated())
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
