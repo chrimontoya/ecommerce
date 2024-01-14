f7f8c984abb41dd88167d6b016b9eb4afe6f8223
Christian montoya <christian.montoya@systemproject.cl>
2024-01-14 00:03:31 -0300

    feat: se agrega entity address

---
+++ ecommerce/src/main/java/cl/grupopi/ecommerce/entities/Address.java
@@ -0,0 +1,38 @@
+package cl.grupopi.ecommerce.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "address", schema = "public")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer number;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+}
