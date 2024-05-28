e8678d45e1a1e8379e7da916429fee59563b0638
Christian Montoya Carrasco <christian.montoya@systemproject.cl>
2024-05-28 01:13:17 -0400

    feat: se agrega tabla base en category-maintainer

--- ecommerce-fe/src/app/app.routes.ts
+++ ecommerce-fe/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { HomeComponent } from './pages/home/home.component';
 import { authGuard } from './guards/auth.guard';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { CategoryMaintainerComponent } from './pages/category-maintainer/category-maintainer.component';
 import { MarkMaintainerComponent } from './pages/mark-maintainer/mark-maintainer.component';
--- ecommerce-fe/src/app/pages/category-maintainer/category-maintainer.component.ts
+++ ecommerce-fe/src/app/pages/category-maintainer/category-maintainer.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-category-maintainer',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatTableModule],
   templateUrl: './category-maintainer.component.html',
-  styleUrl: './category-maintainer.component.scss'
+  styleUrl: './category-maintainer.component.scss',
 })
 export class CategoryMaintainerComponent {
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
+  displayedColumns = [
+    { matColumnDef: 'id_category', label: 'ID' },
+    { matColumnDef: 'category_name', label: 'Nombre Categoria' },
+    { matColumnDef: 'creation_date_category', label: 'Fecha Creación' },
+    { matColumnDef: 'id_sub_category', label: 'ID Sub-Categoria' },
+    { matColumnDef: 'sub_category_name', label: 'Nombre Sub-Categoria' },
+    {
+      matColumnDef: 'creation_date_sub_category',
+      label: 'Fecha Creación Sub-Categoria',
+    },
+  ];
 
+  get columnsName(){
+    return this.displayedColumns.map( (col: any) => col.matColumnDef);
+  }
 }
